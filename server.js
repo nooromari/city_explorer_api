27c5e9395c3e8dd96df4afadc11916aed65a887a
NOOR OMARI <nooromari847@gmail.com>
2021-03-29 23:52:25 +0300

    API

--- server.js
+++ server.js
@@ -2,10 +2,13 @@
 
 require('dotenv').config();
 const express = require('express');
+const superagent = require('superagent');
 const cors = require('cors');
 
 const app=express();
 const PORT = process.env.PORT || 3001;
+const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
 app.use(cors());
 
@@ -40,27 +43,31 @@ function notFoundHandler(request, response) {
 function handleReqLoc(req,res) {
 
   const searchQuery = req.query.city; // localhost:3000/location?city=amman
-  const locationsData = require('./data/location.json');
+  // const locationsData = require('./data/location.json');
+  const url = `https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${searchQuery}&format=json`;
+  superagent.get(url).then(data =>{
+    // try{
+    const location = new Location(searchQuery,data[0]);
+    // console.log(location);
+    res.send(location);
+    // res.status(200).send(location);
+    // }catch(error){
+    //   res.status(500).send(`something ${error}`);
+    // }
 
-  // try{
-  const location = new Location(searchQuery,locationsData[0]);
-  // console.log(location);
-  res.send(location);
-  // res.status(200).send(location);
-  // }catch(error){
-  //   res.status(500).send(`something ${error}`);
-  // }
+  });
 
 }
 
 function handleReqWthr(req,res) {
   const searchQWeather = req.query.city;
-  const weatherData = require('./data/weather.json');
+  // const weatherData = require('./data/weather.json');
+  const url = `https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=${WEATHER_API_KEY}&include=minutely`;
 
   const arrWeather = [];
-
-  weatherData.data.forEach(location =>{
-    let newWeather = new Weather (searchQWeather,location);
+  superagent.get(url).then(data =>{
+  // weatherData.data.forEach(location =>{
+    let newWeather = new Weather (searchQWeather,data[0]);
     arrWeather.push(newWeather);
   });
   res.send(arrWeather);
