301abacb5eb074c426ea4a5abca2aca6b5e65ab4
NOOR OMARI <nooromari847@gmail.com>
2021-03-29 15:12:01 +0300

    location done

--- server.js
+++ server.js
@@ -5,19 +5,43 @@ const express = require('express');
 const cors = require('cors');
 
 const app=express();
-const PORT = process.evn.PORT;
-
-// app.get('/location',handleReq);
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
+
+app.get('/location',handleReqLoc);
+app.get('/weather',handleReqWthr);
+
 app.use('*', (req, res) => {
-  res.send('all good nothing to see here!');
+  res.send('wrong path, nothing to see here!');
 });
 
+function handleReqLoc(req,res) {
 
+  const searchQuery = req.query.city;  // localhost:3000/location?city=amman
+  const locationsData = require('./data/location.json');
 
+  const location = new Location(searchQuery,locationsData[0]);
+  // console.log(location);
+  res.send(location);
+}
 
+function handleReqWthr(req,res) {
+  
+}
+
+function Location(city, cityData) {
+  this.search = city;
+  this.displayAame = cityData.display_name;
+  this.latitude = cityData.lat;
+  this.longitude = cityData.lon;
+  this.type=cityData.type;
+  this.icon=cityData.icon;
+  this.importance=cityData.importance;
+  this.class=cityData.class;
+  
+}
 
 
 app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
