11c039b676d22dee0031049137e37f650b20b1eb
NOOR OMARI <nooromari847@gmail.com>
2021-04-03 02:01:28 +0300

    database deploy

--- schema.sql
+++ schema.sql
@@ -1,9 +1,8 @@
 DROP TABLE IF EXISTS locations;
 
 CREATE TABLE locations (
-    id SERIAL PRIMARY KEY,
-    search_query varchar(255),
-    formatted_query varchar(255),
-    latitude varchar(255),
-    longitude varchar(255)
-);
+    search_query VARCHAR (255) PRIMARY KEY,
+    formatted_query VARCHAR (255),
+    latitude FLOAT,
+    longitude FLOAT
+); 
