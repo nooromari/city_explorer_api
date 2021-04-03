6a4bc0f0874875d834dba5af290aad3a76e59f11
NOOR OMARI <nooromari847@gmail.com>
2021-04-04 01:52:00 +0300

    add yelp and movies

--- server.js
+++ server.js
@@ -11,16 +11,12 @@ const PORT = process.env.PORT || 3005;
 const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 const PARKS_API_KEY = process.env.PARKS_API_KEY;
+const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
+const YELP_API_KEY = process.env.YELP_API_KEY;
+
 const DATABASE_URL = process.env.DATABASE_URL;
-const ENV = process.env.ENV || 'DEB';
 const NODE_ENV = process.env.NODE_ENV;
-// let client ='';
-// if(ENV==='DIV'){
-//   client = new pg.Client({connectionString: DATABASE_URL});
-// }else{client = new pg.Client({
-//   connectionString: DATABASE_URL,
-//   ssl: {rejectUnauthorized: false}
-// });}
+
 const options = NODE_ENV === 'production' ? { connectionString: DATABASE_URL, ssl: { rejectUnauthorized: false } } : { connectionString: DATABASE_URL };
 const client = new pg.Client(options);
 
@@ -33,6 +29,8 @@ app.use(cors());
 app.get('/location', handleReqLoc);
 app.get('/weather', handleReqWthr);
 app.get('/parks', handleReqPar);
+app.get('/movies',handleMovies);
+app.get('/yelp', handleYelp);
 
 app.use('*', handleError); // 404 not found url
 
@@ -101,6 +99,29 @@ function handleReqPar(req, res) {
   });
 }
 
+function handleMovies(req,res) {
+  const search_query = req.query.search_query;
+  const url = `https://api.themoviedb.org/3/movie/550?api_key=${MOVIE_API_KEY}&query=${search_query}`;
+  superagent.get(url).then(mov => {
+    let arr = mov.body.results.map(movData => new Movie(movData));
+    res.status(200).send(arr);
+  }).catch((error) => {
+    res.status(500).send(`something ${error}`);
+  });
+}
+
+function handleYelp(req,res) {
+  const search_query = req.query.search_query;
+  const url = `https://api.yelp.com/v3/businesses/search?location=${search_query}&limit=50&api_key=${YELP_API_KEY}`;
+  superagent.get(url).then(res => {
+    let arr = res.body.businesses.map(reskData => new Resturant(reskData));
+    res.status(200).send(arr);
+  }).catch((error) => {
+    res.status(500).send(`something ${error}`);
+  });
+}
+// .set('Authorization', `Bearer ${YELP_API_KEY}`)
+
 function Location(city, cityData) {
   this.search_query = city;
   this.formatted_query = cityData.display_name;
@@ -121,12 +142,22 @@ function Park(parkData) {
   this.url = parkData.url;
 }
 
-// client.connect().then(()=>app.listen(PORT, () => console.log(`App is listening on ${PORT}`)));
-// app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
+function Movie (data) {
+  this.title = data.title;
+  this.overview = data.overview;
+  this.average_votes = data.vote_average;
+  this.total_votes = data.vote_count;
+  this.image_url= `https://image.tmdb.org/t/p/w500${data.poster_path}`;
+  this.popularity= data.popularity;
+  this.released_on= data.released_on;
+}
 
-// const client = new pg.Client({
-// 	connectionString: process.env.DATABASE_URL,
-// 	ssl: { rejectUnauthorized: false },
-// });
+function Resturant(data) {
+  this.name = data.name;
+  this.image_url = data.image_url;
+  this.price = data.price;
+  this.rating = data.rating;
+  this.url = data.url;
+}
 
 
